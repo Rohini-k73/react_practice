e860a01eaaa1a172848c96f0207ddcb3a495d324
Rohini Kulkarni <kulkarnirohini879@gmail.comgit config --global user.emailgit config --global user.email kulkarnirohini879@gmail.com>
2023-03-01 16:06:16 +0530

    helloworld
    practice
    
    practice

---
+++ helloworld/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          hello
+        </p>
+        <hr></hr>
+        <h1>Hello</h1>
+    
+          
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
